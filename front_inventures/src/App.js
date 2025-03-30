3322749cbaf4e74f21e7cfd995b0d1ca465507ab
Edilsson Bravo <edilsson@gmail.com>
2025-03-30 16:12:21 -0300

    add: base react structure

---
+++ front_inventures/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import fetchData from "./fetchData";
+
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Table from 'react-bootstrap/Table';
+
+const App = () => (
+  <Container fluid className="p-5">
+    <h1 className="header">Inventures URL Shortener</h1>
+    <Row>
+      <Col>a
+      </Col>
+      <Col>b
+      </Col>
+      <Col>c
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default App;
