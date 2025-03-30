06cf50557cc07b7327ee873a2f8514424ab5b225
Edilsson Bravo <edilsson@gmail.com>
2025-03-30 16:12:31 -0300

    add: fetchData component

---
+++ front_inventures/src/fetchData.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export default function fetchData(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading, error };
+}
