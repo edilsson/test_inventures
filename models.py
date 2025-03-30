93df5098367ea056400ec40ff2d20fe6ee5916b0
Edilsson Bravo <edilsson@gmail.com>
2025-03-29 23:28:13 -0300

    chore: use datetime now instead of utcnow in model

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, DateTime
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 
 class ShortenedURL(Base):
     __tablename__ = "shortened_urls"
@@ -8,6 +8,6 @@ class ShortenedURL(Base):
     id = Column(Integer, primary_key=True)
     original_url = Column(String, index=True)
     alias = Column(String, unique=True, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    expires_at = Column(DateTime, default=datetime.utcnow + timedelta(days=3))
+    created_at = Column(DateTime, default=datetime.now(tz=UTC))
+    expires_at = Column(DateTime, default=datetime.now(tz=UTC) + timedelta(days=3))
     clicks = Column(Integer, default=0)
