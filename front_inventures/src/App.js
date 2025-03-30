59d0cf497c4982046e576272a5ac575d15b1300f
Edilsson Bravo <edilsson@gmail.com>
2025-03-30 17:15:31 -0300

    add: shortener component

--- front_inventures/src/App.js
+++ front_inventures/src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import fetchData from "./fetchData";
+import Shortener from "./Shortener";
 
 import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
@@ -13,8 +12,9 @@ const App = () => (
   <Container fluid className="p-5">
     <h1 className="header">Inventures URL Shortener</h1>
     <Row>
-      <Col>a
-      </Col>
+        <Shortener />
+    </Row>
+    <Row>
       <Col>b
       </Col>
       <Col>c
